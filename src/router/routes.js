b43e0786f9c56928a9d4b25bedb7ee7ab02bd281
Marco Antônio <marcoantonio959z@gmail.com>
2025-06-17 18:29:12 -0300

    Update routes.js
    Adicionando Rota para HomePages.vue

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,11 +3,13 @@ export default [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
    children: [
+  { path: '', component: () => import('pages/HomePage.vue') },
   { path: '', redirect: '/create' },
   { path: 'create', component: () => import('pages/CreateNote.vue') },
   { path: 'notes', component: () => import('pages/NotesList.vue') },
   { path: 'notes/edit/:id', component: () => import('pages/EditNote.vue') },
-  { path: 'calendar', component: () => import('pages/NoteCalendar.vue') }
+  { path: 'calendar', component: () => import('pages/NoteCalendar.vue') },
+
 ]
 
   }
